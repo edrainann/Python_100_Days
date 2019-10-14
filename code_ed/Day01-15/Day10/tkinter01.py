265dea30d68614d21579199ce3f3e6b4e2901242
edrain <438748968@qq.com>
2019-10-14 14:57:25 +0800

    Day10 tkinter做最简单的GUI

--- code_ed/Day01-15/Day10/tkinter01.py
+++ code_ed/Day01-15/Day10/tkinter01.py
@@ -2,6 +2,16 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2019/10/14
 # @Author  : Edrain
+"""
+基本上使用tkinter来开发GUI应用需要以下5个步骤：
+导入tkinter模块中我们需要的东西。
+创建一个顶层窗口对象并用它来承载整个GUI应用。
+在顶层窗口对象上添加GUI组件。
+通过代码将这些GUI组件的功能组织起来。
+进入主事件循环(main loop)。
+Tk为控件的摆放提供了三种布局管理器，通过布局管理器可以对控件进行定位，这三种布局管理器分别是：
+Placer（开发者提供控件的大小和摆放位置）、Packer（自动将控件填充到合适的位置）和Grid（基于网格坐标来摆放控件）
+"""
 import tkinter
 import tkinter.messagebox
 
