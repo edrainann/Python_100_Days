6d67f3be9ea6f1bb49fd22d12454e953fc03430f
edrain <438748968@qq.com>
2019-10-23 16:01:56 +0800

    Day14 python 发送邮件+带有附件的邮件

--- code_ed/Day01-15/code/Day14/request01.py
+++ code_ed/Day01-15/code/Day14/request01.py
@@ -26,7 +26,7 @@ def main():
     下面的代码中使用了天行数据接口提供的网络API
     要使用该数据接口需要在天行数据的网站上注册
     然后用自己的Key替换掉下面代码的中APIKey即可"""
-    resp = requests.get('http://api.tianapi.com/meinv/?key=a3b60fe2bc4f3908644516fb1c544bc7&num=3')
+    resp = requests.get('http://api.tianapi.com/meinv/?key=APIKey&num=3')
     # 将服务器返回的JSON格式的数据解析为字典
     data_model = resp.json()
     for mm_dict in data_model['newslist']:
