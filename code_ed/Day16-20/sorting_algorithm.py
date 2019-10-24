f2b1712cdb571147f66046a33919ae0da502bfd2
edrain <438748968@qq.com>
2019-10-24 10:32:32 +0800

    Day16 高质量冒泡排序

--- code_ed/Day16-20/sorting_algorithm.py
+++ code_ed/Day16-20/sorting_algorithm.py
@@ -19,6 +19,7 @@ def select_sort(origin_items, comp=lambda x, y: x < y):
     第一趟：2 11 9 4
     第二趟：2 4 11 9
     第三趟：2 4 9 11
+    选择排序存在一个明显的问题，就是它的【不稳定性】。当数列包含多个相等的元素时，选择排序有可能打乱他们的原有顺序。
     """
     items = origin_items[:]
     for i in range(len(items) - 1):
@@ -27,7 +28,30 @@ def select_sort(origin_items, comp=lambda x, y: x < y):
             if comp(items[j], items[min_index]):
                 min_index = j
         items[i], items[min_index] = items[min_index], items[i]
-        print(f'这是{i+1}趟', items)
+        print(f'这是{i + 1}趟', items)
+    return items
+
+
+def bubble_sort(origin_items, comp=lambda x, y: x > y):
+    """高质量冒泡排序（搅拌排序）
+    虽然存在一定的性能缺陷，却是一个稳定排序。
+    所以说算法没有绝对的好坏之分，关键要看应用场景。
+    """
+    items = origin_items[:]
+    for i in range(len(items) - 1):
+        swapped = False
+        for j in range(i, len(items) - 1 - i):
+            if comp(items[j], items[j + 1]):
+                items[j], items[j + 1] = items[j + 1], items[j]
+                swapped = True
+        if swapped:
+            swapped = False
+            for j in range(len(items) - 2 - i, i, -1):
+                if comp(items[j - 1], items[j]):
+                    items[j], items[j - 1] = items[j - 1], items[j]
+                    swapped = True
+        if not swapped:
+            break
     return items
 
 
