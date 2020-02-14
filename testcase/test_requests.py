4a67a8e95f9078b8652795c91e3e3ab21930634c
edrainann <438748968@qq.com>
2020-02-14 15:55:09 +0800

    错误的通过params发送post请求

--- testcase/test_requests.py
+++ testcase/test_requests.py
@@ -9,10 +9,23 @@ import logging
 
 class TestRequests(object):
     logging.basicConfig(level=logging.INFO)
+    url = "https://testerhome.com/api/v3/topics.json?limit=2"
 
     def test_get(self):
-        r = requests.get("https://testerhome.com/api/v3/topics.json?limit=2")
+        r = requests.get(self.url)
+        # r = requests.get("https://www.baidu.com")  # 返回的内容不是json格式的
         logging.info(r)  # <class 'requests.models.Response'>
         logging.info(r.text)  # <class 'str'>
         logging.info(r.json())  # <class 'dict'>
         logging.info(json.dumps(r.json(), indent=2))  # 把dict对象dumps成字符串,<class 'str'>
+
+    def test_post(self):
+        r = requests.post(self.url,
+                          params={"a": 1, "b": "string content"},
+                          proxies={"https": "127.0.0.1:8889",
+                                   "http": "127.0.0.1:8889"},  # 启动charles走代理
+                          verify=False  # 关闭认证TLS证书
+                          )
+        logging.info(r.url)
+        logging.info(r.text)
+        logging.info(json.dumps(r.json(), indent=2))
