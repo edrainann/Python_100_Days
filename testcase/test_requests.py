e91d644797d147710d194b65521d86db8f95069a
edrainann <438748968@qq.com>
2020-02-14 15:17:10 +0800

    Serialize ``obj`` to a JSON formatted ``str``.

--- testcase/test_requests.py
+++ testcase/test_requests.py
@@ -12,7 +12,7 @@ class TestRequests(object):
 
     def test_get(self):
         r = requests.get("https://testerhome.com/api/v3/topics.json?limit=2")
-        logging.info(r)
-        logging.info(r.text)
-        logging.info(r.json())
-        logging.info(json.dumps(r.json(), indent=2))  # 把对象dumps成字符串
+        logging.info(r)  # <class 'requests.models.Response'>
+        logging.info(r.text)  # <class 'str'>
+        logging.info(r.json())  # <class 'dict'>
+        logging.info(json.dumps(r.json(), indent=2))  # 把dict对象dumps成字符串,<class 'str'>
