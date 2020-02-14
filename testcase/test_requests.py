5adae9ec22cde78c19f7123acb04cbbcf3fb6f12
edrainann <438748968@qq.com>
2020-02-14 15:06:12 +0800

    把对象dumps成json

--- testcase/test_requests.py
+++ testcase/test_requests.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2020/2/14 
 # @Author  : Edrain
+import json
+
 import requests
 import logging
 
@@ -13,3 +15,4 @@ class TestRequests(object):
         logging.info(r)
         logging.info(r.text)
         logging.info(r.json())
+        logging.info(json.dumps(r.json(), indent=2))  # 把对象dumps成字符串
