0d223a5e15fff0b7b293e9493e21e733957b2398
edrainann <438748968@qq.com>
2020-03-17 23:09:36 +0800

    re.match(condition, str) a=re.compile(condition) a.match(str)

--- code_ed/Day01-15/code/Day01/re01.py
+++ code_ed/Day01-15/code/Day01/re01.py
@@ -50,9 +50,23 @@ def re_split02(need_regular_string):
     return re.split(re_condition, need_regular_string)
 
 
+def re_compile(need_regular_string):
+    """
+    当我们在Python中使用正则表达式时，re模块内部会干两件事情：
+    编译正则表达式，如果正则表达式的字符串本身不合法，会报错；
+    用编译后的正则表达式去匹配字符串。
+    如果一个正则表达式要重复使用几千次，出于效率的考虑，我们可以预编译该正则表达式，
+    接下来重复使用时就不需要编译这个步骤了，直接匹配：
+    """
+    re_condition = r"^(\d{3})-(\d{3,9})$"
+    re_phone = re.compile(re_condition)
+    return re_phone.match(need_regular_string).groups()
+
+
 if __name__ == "__main__":
-    need_regular_string01 = "111-0922333a"
+    need_regular_string01 = "111-0922333"
     need_regular_string02 = "a b   c"
     re_match01(need_regular_string01)
     print(re_split01(need_regular_string01))
     print(re_split02(need_regular_string02))
+    print(re_compile(need_regular_string01))
